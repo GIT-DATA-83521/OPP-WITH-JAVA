ede779867406faa4dc07d27aabd1fbabbe1e62ef
kd4-shantanu-83521 <ahirebhagesh1@gmail.com>
2024-04-11 15:05:11 +0530

    Assignment 04

---
+++ Assignment_04/Fruits/src/com/app/fruits/Fruits.java
@@ -0,0 +1,58 @@
+package com.app.fruits;
+
+public class Fruits
+{
+	private double weight;
+	private String color;
+	private boolean isfresh;
+	private String name;
+	
+	
+	
+	public Fruits()
+	{
+		super();
+	}
+	public Fruits(double weight, String color, boolean isfresh, String name)
+	{
+		super();
+		this.weight = weight;
+		this.color = color;
+		this.isfresh = isfresh;
+		this.name = name;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public boolean isIsfresh() {
+		return isfresh;
+	}
+	public void setIsfresh(boolean isfresh) {
+		this.isfresh = isfresh;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Fruit [weight=" + weight + ", color=" + color + ", isfresh=" + isfresh + ", name=" + name + "]";
+	}
+	
+	public String taste() 
+	{
+		return "no specific taste";
+	}
+}
